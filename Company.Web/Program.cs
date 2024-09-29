27df7da927c9813848297581099f1d7694ed9ea8
Amir <Amir@DESKTOP-VLHUF4M>
2024-09-29 23:10:54 +0300

    IdentitySetup&Migration

--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -6,6 +6,7 @@ using Company.Services.Interfaces;
 using Company.Services.Services;
 using Company.Services.Mapping;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace Company.Web
 {
@@ -33,6 +34,31 @@ namespace Company.Web
             //builder.Services.AddScoped<IGenirecRepository<Employee>, GenirecRepository<Employee>>();
             //builder.Services.AddScoped<IGenirecRepository<Department>, GenirecRepository<Department>>();
 
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(config =>
+            {
+                config.Password.RequiredUniqueChars = 2;
+                config.Password.RequireDigit = true;
+                config.Password.RequireLowercase = true;
+                config.Password.RequireUppercase = true;
+                config.Password.RequireNonAlphanumeric= true;
+                config.User.RequireUniqueEmail = true;
+                config.Lockout.AllowedForNewUsers = true;
+                config.Lockout.MaxFailedAccessAttempts = 3;
+                config.Lockout.DefaultLockoutTimeSpan= TimeSpan.FromHours(1);
+
+            }).AddEntityFrameworkStores<CompanyDbContext>()
+            .AddDefaultTokenProviders();
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                options.SlidingExpiration = true;
+                options.LogoutPath = "/Account/Logout";
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -47,6 +73,8 @@ namespace Company.Web
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 

4826d7ca19dc2433756102daf39cadc48e2748ed
Amir <Amir@DESKTOP-VLHUF4M>
2024-10-06 15:28:10 +0300

    AccountController(SignIn)

---
+++ Company.Web/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Format")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}

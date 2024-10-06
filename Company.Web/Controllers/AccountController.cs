29b5e23d3389b6f48ad6fa3fd04bbbbd172dd747
Amir <Amir@DESKTOP-VLHUF4M>
2024-10-06 16:59:09 +0300

    AccountController(ResetPassword)

--- Company.Web/Controllers/AccountController.cs
+++ Company.Web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Company.Data.Models;
+﻿using Company.Data.Migrations;
+using Company.Data.Models;
 using Company.Services.Helper;
 using Company.Web.Models;
 using Microsoft.AspNetCore.Identity;
@@ -121,6 +122,34 @@ namespace Company.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult ResetPassword(string Email , string Token)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user is not null)
+                {
+                    var Res = await _userManager.ResetPasswordAsync(user,model.Token,model.Password);
+                    if(Res.Succeeded)
+                    {
+                        return RedirectToAction(nameof(SignIn));
+                    }
+
+                    foreach (var err in Res.Errors)
+                    {
+                        ModelState.AddModelError("", err.Description);
+                    }
+                }
+            }
+                    return View(model);
+        } 
 
     }
 }
---
+++ Company.Web/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$", ErrorMessage = "Password Must be atleast 8 Characters")]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        [Compare(nameof(Password), ErrorMessage = "Confirm Password dosn`t match Password")]
+        [Required(ErrorMessage = "Confirm Password is required")]
+        public string ConfirmPassword { get; set; }
+
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}

a7bcc3ec9abe142d731a8fd12f664367ef76865d
Amir <Amir@DESKTOP-VLHUF4M>
2024-10-08 21:16:12 +0300

    UserController(Update&DetailsCont)

--- Company.Web/Controllers/UserController.cs
+++ Company.Web/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Company.Data.Models;
+using Company.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -38,7 +39,58 @@ namespace Company.Web.Controllers
                 return NotFound();
             }
 
+            if (viewname == "Update")
+            {
+                var userModel = new UserUpdateViewModel
+                {
+                    UserName = user.UserName,
+                    id = user.Id
+                };
+                return View(viewname, userModel);
+            }
+
             return View(viewname,user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(string? id)
+        {
+            return await Details(id, "Update");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(string? id , UserUpdateViewModel appUserUpdate)
+        {
+            if (id != appUserUpdate.id)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    var user = await _userManager.FindByIdAsync (id);
+                    if (user is null)
+                    {
+                        return NotFound();
+                    }
+                    user.UserName = appUserUpdate.UserName;
+                    user.NormalizedUserName = appUserUpdate.UserName.ToUpper();
+
+                    var res = await _userManager.UpdateAsync(user);
+                    if (res.Succeeded)
+                    {
+                        _logger.LogInformation("User Update Successfuiiy");
+                        return RedirectToAction(nameof(Index));
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogInformation(ex.Message);
+                }
+            }
+            return View(appUserUpdate);
         }
     }
 }
